1487de5767349349d9546431ef1a02f2da1c2a5e
Jakub <jakubpodgorski5@gmail.com>
2022-06-16 14:18:26 +0200

    improved code, trying to not use innerHTML

--- script.js
+++ script.js
@@ -8,22 +8,27 @@ Form.addEventListener("submit", addNewComment);
 function addNewComment(e){
     e.preventDefault();
     var addNewComment = {
-        "name": document.getElementById('username').value,
-        "comment": document.getElementById('content').value
-    } 
+        username: document.getElementById('username').value,
+        content: document.getElementById('content').value
+    }
+    console.log(addNewComment)
 
     var date = new Date()
     var dateTime = date.getDate() + "/" + (date.getMonth()+1) + "/" + date.getFullYear() + " " + date.getHours() + ":" + (date.getMinutes()<10?'0':'') + date.getMinutes(); // ? = if...else
 
-    var newCommentInfo = commentTemplate.innerHTML.replace('{{name}}',addNewComment.name);
-    newCommentInfo = newCommentInfo.replace('{{comment}}',addNewComment.comment);
+    var newCommentInfo = commentTemplate.innerHTML.replace('{{name}}',addNewComment.username);
     newCommentInfo = newCommentInfo.replace('{{dateTime}}',dateTime);
+    newCommentInfo = newCommentInfo.replace('{{content}}',addNewComment.content);
+    console.log(newCommentInfo)
 
     var newComment = document.createElement('div');
     newComment.classList.add('user-comment'); // assigns 'user-comment' class to div previously created
-    newComment.innerHTML = newCommentInfo; // changes placeholders to content from addNewComment variable
+    newComment.textContent = newCommentInfo; // changes placeholders to content from addNewComment variable
     
-    if(addNewComment.name.length > 0 && addNewComment.comment.length > 0){ // checks if form provided name and content
+    if(addNewComment.username.length > 0 && addNewComment.content.length > 0){ // checks if form provided name and content
         commentsList.appendChild(newComment); // creates new div on the end
     }
 }
+
+// use textContent instead of innerHTML because it's less exploitable for xss attacks
+// example: post <img src="x" onerror='alert("xd")'/> in comment content when there's innerHTML used
